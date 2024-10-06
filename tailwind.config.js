c8d71fdfde6ce26dd4cbf8299e42e0ea5d7f86b4
CTRLPreviously <andreinichita888@gmail.com>
2024-10-06 13:59:28 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  mode: 'jit',
+  purge: [
+    '*.html',
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+
+  corePlugins: {
+    preflight: false,
+  },
+
+  prefix: 'tw-',
+
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
